4feaa4114c8a59e0a2cd6e5189618df6314a5a4c
Fabio Marayo <binhofvieira@gmail.com>
2017-09-22 23:04:51 -0300

    sessao 10, aula 43

--- config/server.js
+++ config/server.js
@@ -1,13 +1,17 @@
 var express = require('express');
 var consign = require('consign');
 var app     = express();
+
 var bodyParser = require('body-parser');
+var expressValidator = require('express-validator');
 
 app.set('view engine', 'ejs');
 app.set('views', './app/views');
 app.use(bodyParser.urlencoded({
     extended: true
 }));
+app.use(expressValidator());
+
 consign()
     .include('./app/routes')
     .then('./config/dbConnection.js')
