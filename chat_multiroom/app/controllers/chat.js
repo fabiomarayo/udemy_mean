44b7d2a5c1c5d405f05b1781a433b0d1070eac46
Fabio Marayo <binhofvieira@gmail.com>
2017-09-23 13:39:28 -0300

    sessao 13, aula 66

--- chat_multiroom/app/controllers/chat.js
+++ chat_multiroom/app/controllers/chat.js
@@ -1,3 +1,14 @@
-module.exports.iniciaChat = function(app, req, res){
-     res.render('chat');
+module.exports.iniciaChat = function(application, req, res){
+    var data = req.body;
+
+    req.assert('apelido', 'Apelido é obrigatório').notEmpty();
+    req.assert('apelido', 'Apelido deve ter entre 3 e 15 caracteres').len(3, 15);
+    
+    var errors = req.validationErrors();
+    if(errors) {
+        res.render('index', {errors:errors});
+        return;
+    }
+
+    res.render('chat');
 }
--- chat_multiroom/index.js
+++ chat_multiroom/index.js
@@ -1,7 +1,18 @@
 // importar configurações do servidor
 var app = require('./config/server');
 
-// parametrizar porta de escuta
-app.listen(80, function() {
+// parametrizar porta de escurta
+var server = app.listen(80, function() {
     console.info('Servidor Online');
+});
+
+var io = require("socket.io").listen(server);
+
+/* criar a conexao por websocket */
+io.on('connection', function(socket) {
+    console.log('Usuario Conectou');
+
+    socket.on('disconnect',function() {
+        console.log('usuario desconectou');
+    });
 });
