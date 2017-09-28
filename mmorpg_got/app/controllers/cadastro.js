24300be9da16b12eb12beba430b55a37f79b916d
Fabio Marayo <binhofvieira@gmail.com>
2017-09-28 19:20:47 -0300

    aaula 87

---
+++ mmorpg_got/app/controllers/cadastro.js
@@ -0,0 +1,21 @@
+module.exports.cadastro = function(application, req, res) {
+    res.render('cadastro', {dados: {}, validacao:{}});
+}
+
+module.exports.cadastrar = function(application, req, res) {
+    var dados = req.body;
+    
+    req.assert('nome', 'Nome nao pode ser vazio').notEmpty();
+    req.assert('usuario', 'Usuario nao pode ser vazio').notEmpty();
+    req.assert('senha', 'Senha nao pode ser vazio').notEmpty();
+    req.assert('casa', 'Casa nao pode ser vazio').notEmpty();
+
+    var erros = req.validationErrors();
+    if(erros) {
+        res.render('cadastro', {validacao: erros, dados: dados});
+        return;
+    }
+
+    res.send("OK");
+
+}
